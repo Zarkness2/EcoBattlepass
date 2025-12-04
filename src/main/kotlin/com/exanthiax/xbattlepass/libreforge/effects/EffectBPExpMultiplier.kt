4f20d344e5ea11254adc739ffff12e42ceb8f408
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2025-12-04 00:26:16 +0000

    fixed battlepass_xp_multiplier

--- src/main/kotlin/com/exanthiax/xbattlepass/libreforge/effects/EffectBPExpMultiplier.kt
+++ src/main/kotlin/com/exanthiax/xbattlepass/libreforge/effects/EffectBPExpMultiplier.kt
@@ -9,22 +9,22 @@ import com.exanthiax.xbattlepass.battlepass.BattlePass
 import com.exanthiax.xbattlepass.battlepass.BattlePasses
 
 object EffectBPExpMultiplier : MultiMultiplierEffect<BattlePass>("battlepass_xp_multiplier") {
-    @EventHandler(ignoreCancelled = true)
-    fun handle(event: PlayerBPExpGainEvent) {
-        val player = event.player
+    override val key = "battlepasses"
 
-        if (event.isMultiply) {
-            event.setAmount(event.getAmount() * getMultiplier(player.toDispatcher(), event.battlepass))
-        }
+    override fun getElement(key: String): BattlePass? {
+        return BattlePasses.getByID(key)
     }
-
-    override val key: String = "battlepasses"
 
     override fun getAllElements(): Collection<BattlePass> {
         return BattlePasses.values()
     }
 
-    override fun getElement(key: String): BattlePass? {
-        return BattlePasses.getByID(key)
+    @EventHandler(ignoreCancelled = true)
+    fun handle(event: PlayerBPExpGainEvent) {
+        val player = event.player
+
+//        if (event.isMultiply) {
+            event.setAmount(event.getAmount() * getMultiplier(player.toDispatcher(), event.battlepass))
+        }
     }
-}
+//}
